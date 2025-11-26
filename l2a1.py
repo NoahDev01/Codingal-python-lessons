cecc5aeb24829812a57c4ee2193507872fb23c2d
NoahDev01 <aamysan@outlook.com>
2025-11-26 15:36:42 +0100

    l2a1

---
+++ l2a1.py
@@ -0,0 +1,29 @@
+a=5
+print("type of a:",type(a))
+
+b=2.5
+print("type of b:",type(b))
+
+c=True
+print("type of c:",type(c))
+
+d="Noah"
+print("type of d:",type(d))
+
+a=str(a)
+print("type of a",type(a))
+
+b=int(b)
+print("type of b",type(b))
+
+c=float(c)
+print("type of c",type(c))
+
+text=input("enter a string")
+ 
+revtext=text[::-1]
+print("reversed string is:",revtext)
+
+
+
+
