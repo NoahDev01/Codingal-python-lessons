7d43b13e47544c8e03a26120ae47d3db5d1a0524
NoahDev01 <aamysan@outlook.com>
2025-11-28 16:09:45 +0100

    lesson4a3

---
+++ l4a1.py
@@ -0,0 +1,12 @@
+tree1=89
+tree2=67
+tree3=41
+tree4=56
+tree5=91
+sum=tree1+tree2+tree3+tree4+tree5
+average=sum/5
+print("sum : ",sum)
+print("average : ",average)
+
+
+
---
+++ l4a2.py
@@ -0,0 +1,11 @@
+print("enter the marks for 4 subjects")
+math=int(input("maths:"))
+english=int(input("english:"))
+science=int(input("science:"))
+history=int(input("history:"))
+netherlands=int(input("netherlands:"))
+sum=math+english+science+history+netherlands
+percentage=(sum/400)*100
+print("sum : ",sum)
+print("percentage : ",percentage)
+
