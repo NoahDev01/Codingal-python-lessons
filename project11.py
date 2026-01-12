161d92c040393615647b1a9f08ca975f9724daa0
NoahDev01 <aamysan@outlook.com>
2026-01-12 17:23:36 +0100

    p11

---
+++ project11.py
@@ -0,0 +1,9 @@
+bill_amount = float(input("Enter the total bill amount:))
+amount_paid = float(input("Enter the amount paid: "))
+due_amount = bill_amount - amount_paid
+if due_amount > 0:
+    print(f"Customer due amount is: {due_amount}")
+elif due_amount == 0:
+    print("The bill is fully paid. No due amount.")
+else:
+    print(f"Extra amount paid: {due_amount}")
