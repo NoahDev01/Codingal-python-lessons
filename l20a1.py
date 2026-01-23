ab95fa250b314b3f432a4b0de923601b043aa26c
NoahDev01 <aamysan@outlook.com>
2026-01-23 16:19:33 +0100

    l20

---
+++ l20a1.py
@@ -0,0 +1,14 @@
+tuplex=("truple",False,1,2,3)
+print(tuplex)
+tuplex=(8,3,6,5,1,4,6,7)
+print(tuplex)
+tuplex=tuplex+(9,)
+print(tuplex)
+truple1=(60,70,10,50,50)
+print(truple1.count(50))
+tuplex=(8,9,3,4,2,1,7,6)
+_slice=tuplex[3:5]
+print(_slice)
+slice=tuplex[:6]
+print(slice)
+
